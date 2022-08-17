fa42ffcb408ea725eee5dc0a20acc58aa5e4f53e
Khali Gopaul <khali.gopaul@gmail.com>
2022-08-17 13:04:16 -0400

    added photos

--- src/routers/photo-router.ts
+++ src/routers/photo-router.ts
@@ -1,10 +1,38 @@
 import { Router, Request, Response } from "express";
-import { getAllPhotos } from "../services/photo-services";
+import {  PhotoServices } from "../services/photo-services";
 
-export const photoRouter = Router()
+export const photoRouter = Router();
 
 photoRouter.get('/', async (req: Request, res: Response ) =>{
-const results = await getAllPhotos();
+const results = await PhotoServices.getAllPhotos();
 
 res.status(200).send(results);
 });
+
+photoRouter.post('/', async (req: Request, res: 
+    Response) => {
+const { photoUrl, description} = req.body;
+
+if(!photoUrl) {
+    res.status(400).send("PhotoUrl is required");
+    return;
+}
+
+    const insertId = await PhotoServices.createPhoto({ 
+        photoUrl, description });
+
+res.status(201).send({ insertId });
+});
+
+photoRouter.patch('/:id', async (req: Request, res: Response) => {
+    const {id} = req.params
+    const {likes} = req.body
+
+    if(!likes){
+        res.status(400).send("Likes are required.")
+    }
+
+    const photo = await PhotoServices.updateLikes(id, likes)
+
+    res.status(200).json(photo)
+})
--- src/services/photo-services.ts
+++ src/services/photo-services.ts
@@ -1,18 +1,50 @@
-import { db } from '../db/db';
+import { ObjectId } from "mongodb";
+import { db } from "../db/db";
 import { Photo } from "../models/photo";
 
 interface PhotoServices {
-    updateLikes(id:string, inc:number): 
-    Promise<Photo>
-    createphoto(photo:Photo): Promise<string>;
-    createComment(id:string, comment:string): Promise<Photo>;
-    getAllPhotos():Promise<Photo[]>;
+  updateLikes(id: string, inc: number): Promise<Photo>;
+  //   createComment(id: string, comment: string): Promise<Photo>;
+  createPhoto(photo: Photo): Promise<string>;
+  getAllPhotos(): Promise<Photo[]>;
 }
 
-const photoCollection = db.collection<Photo>('photos');
+const photoCollection = db.collection<Photo>("photos");
 
 export const getAllPhotos = async (): Promise<Photo[]> => {
-    const photos = await photoCollection.find().toArray();
+  const photos = await photoCollection.find().toArray();
 
-    return photos;
+  return photos;
+};
+
+export const createPhoto = async (photo: Photo): Promise<string> => {
+  try {
+    const res = await photoCollection.insertOne(photo);
+    return res.insertedId.toString();
+  } catch (error) {
+    return "Something went wrong";
+  }
+};
+
+export const updateLikes = async (
+  id: string,
+  inc: number = 1
+): Promise<Photo> => {
+  const res = await photoCollection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $inc: { likes: inc } }
+  );
+
+  const updatedPhoto = res.value as Photo;
+
+  return updatedPhoto;
+};
+
+// export const createComment = async (params: type) => {};
+
+export const PhotoServices: PhotoServices = {
+  getAllPhotos,
+  createPhoto,
+  updateLikes,
+  // createComment,
 };
